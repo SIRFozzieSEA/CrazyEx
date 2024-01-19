8d1d1bcf35194cc05e75394800766c8d79ea26df
SIRFozzieSEA <github@codef.com>
2024-01-18 22:12:12 -0800

    Upgraded and cleaned

--- app/src/main/java/com/codef/crazyex/MainActivity.java
+++ app/src/main/java/com/codef/crazyex/MainActivity.java
@@ -1,14 +1,9 @@
 package com.codef.crazyex;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
 import android.view.View.OnClickListener;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.media.MediaPlayer;
 
@@ -19,58 +14,58 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button_biteyou);
+        Button button = findViewById(R.id.button_biteyou);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_delish);
+        button = findViewById(R.id.button_delish);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_disgust);
+        button = findViewById(R.id.button_disgust);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_donteven);
+        button = findViewById(R.id.button_donteven);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_eeeeh);
+        button = findViewById(R.id.button_eeeeh);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_growl);
+        button = findViewById(R.id.button_growl);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_growlkitty);
+        button = findViewById(R.id.button_growlkitty);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_hey);
+        button = findViewById(R.id.button_hey);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_noodles);
+        button = findViewById(R.id.button_noodles);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_nooo);
+        button = findViewById(R.id.button_nooo);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_punk);
+        button = findViewById(R.id.button_punk);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_puss);
+        button = findViewById(R.id.button_puss);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_pussyface);
+        button = findViewById(R.id.button_pussyface);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_saranara);
+        button = findViewById(R.id.button_saranara);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_schmee);
+        button = findViewById(R.id.button_schmee);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_whatthecrap);
+        button = findViewById(R.id.button_whatthecrap);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_whosthekiki);
+        button = findViewById(R.id.button_whosthekiki);
         button.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button_whydyoudoit);
+        button = findViewById(R.id.button_whydyoudoit);
         button.setOnClickListener(this);
 
     }
